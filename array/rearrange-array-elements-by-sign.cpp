312590ef41d19d6fcdf0d46cb84313bc3138f689
KAMAL-02 <154740582+KAMAL-02@users.noreply.github.com>
2024-02-25 20:04:39 +0530

    Create rearrange-array-elements-by-sign.cpp

---
+++ array/rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,21 @@
+//249
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size() ;
+        vector<int>v(n,0);
+        int posIndex = 0 ;
+        int negIndex = 1 ;
+        for(int i = 0 ; i < n ;i++){
+            if(nums[i] > 0){
+                v[posIndex] = nums[i] ;
+                posIndex+=2 ;
+            }else{
+                v[negIndex] = nums[i] ;
+                negIndex+=2 ;
+            }
+        }
+        return v ;
+        
+    }
+};
